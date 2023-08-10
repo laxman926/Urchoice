bf54798f93750f941c0921f437e4426b738e6a2e
laxman <kumarlaxman926@gmail.com>
2023-08-09 23:13:54 -0400

    init and home screen

---
+++ src/screens/Home/index.js
@@ -0,0 +1,47 @@
+import React, {useState, useEffect} from 'react';
+import {View, StyleSheet, Text, ImageBackground, Pressable} from 'react-native';
+import styles from './styles';
+import Fontisto from "react-native-vector-icons/Fontisto";
+
+
+// import products from '../../data/products';
+
+const HomeScreen = () => {
+  
+
+ 
+
+  return (
+    <View >
+         <Pressable 
+     style={styles.searchButton}
+      onPress={ () => console.warn('Search Btn clicked')}
+        >
+            <Fontisto name="search" size={25} color={"#f15454"} />
+        <Text style={styles.searchButtonText}>Where are you going</Text>
+        </Pressable>
+
+
+        <ImageBackground source={require('../../../assets/images/wallpaper.jpg')} style={styles.image}>
+        <Text style={styles.title}> Go Near</Text>
+
+        <Pressable 
+     style={styles.button}
+      onPress={ () => console.warn('Explore Btn clicked')}
+        >
+        <Text style={styles.buttonText}>Explore near by stays</Text>
+        </Pressable>
+            
+        </ImageBackground>
+    
+   
+
+    
+
+    </View>
+  );
+};
+
+
+
+export default HomeScreen;
